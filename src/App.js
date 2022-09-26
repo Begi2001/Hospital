a042bfd1f9794a1f1347d619e2acb7c0cad99573
Bek8866 <begi20010202@gmail.com>
2022-09-26 11:19:22 +0500

    feat:faq

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
                   <Route path='/service/:id' element={<Service/>}/>
                   <Route path='/services' element={<Services/>}/>
               </Routes>
-              {/*<Footer/>*/}
+              <Footer/>
           </>
     );
 }
